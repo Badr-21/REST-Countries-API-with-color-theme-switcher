59ccc40adac4240a76263f0d26e3f4a1af711b00
Badreddine <boudaoud.badreddine@gmail.com>
2023-03-21 21:44:31 +0100

    fix files

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createContext, useState } from "react";
+import "./App.css";
+import { Container } from "./components/Container";
+import { Header } from "./components/Header";
 
+export const darkModeContext = createContext();
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+   const [darkMode, setDarkMode] = useState(false);
+   return (
+      <div className="App">
+         <darkModeContext.Provider value={{ darkMode, setDarkMode }}>
+            <Header />
+            <Container />
+         </darkModeContext.Provider>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+   );
 }
 
-export default App
+export default App;
